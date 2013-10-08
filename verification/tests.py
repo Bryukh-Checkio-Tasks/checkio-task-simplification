beabb22b9a1a070c6f2c1cc811bdef56b158edc1
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-08 15:52:58 +1200

    referee and tests

--- verification/tests.py
+++ verification/tests.py
@@ -11,26 +11,96 @@ Each test is dict with
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": "(x-1)*(x+1)",
+            "answer": "x*x-1"
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": "(x+1)*(x+1)",
+            "answer": "x*x+2*x+1"
+        },
+        {
+            "input": "(x+3)*x*2-x*x",
+            "answer": "x*x+6*x"
+        },
+
+        {
+            "input": "x+x*x+x*x*x",
+            "answer": "x*x*x+x*x+x"
+        },
+        {
+            "input": "(2*x+3)*2-x+x*x*x*x",
+            "answer": "x*x*x*x+3*x+6"
+        },
+
+        {
+            "input": "3+4*x-2-2*x",
+            "answer": "2*x+1"
+        },
+
+        {
+            "input": "x*(x+1)*(x+1)",
+            "answer": "x*x*x+2*x*x+x"
+        },
+
+        {
+            "input": "x*x*x*x*x+10-2",
+            "answer": "x*x*x*x*x+8"
+        },
+        {
+            "input": "(x-100)*(x+100)",
+            "answer": "x*x-10000"
+        },
+        {
+            "input": "(x+2)*(x+2)*(x+2)",
+            "answer": "x*x*x+6*x*x+12*x+8"
         }
-    ],
+
+],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": "(x-1)*(x-1)",
+            "answer": "x*x-2*x+1"
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
+            "input": "(x+3)*(x+3)",
+            "answer": "x*x+6*x+9"
+        },
+        {
+            "input": "(x+4)*x*3-x*x",
+            "answer": "2*x*x+12*x"
+        },
+
+        {
+            "input": "x+x*x+x*x*x*x",
+            "answer": "x*x*x*x+x*x+x"
+        },
+        {
+            "input": "2*(2*x+3)-x+x*x*x*x",
+            "answer": "x*x*x*x+3*x+6"
+        },
+
+        {
+            "input": "3+4*x-24-2*x",
+            "answer": "2*x-21"
+        },
+
+        {
+            "input": "(x+1)*x*(x+1)",
+            "answer": "x*x*x+2*x*x+x"
+        },
+
+        {
+            "input": "x*x*x*x*x+100-2",
+            "answer": "x*x*x*x*x+98"
+        },
+        {
+            "input": "(x-500)*(x+500)",
+            "answer": "x*x-250000"
+        },
+        {
+            "input": "(x-2)*(x+2)*(x+2)",
+            "answer": "x*x*x+2*x*x-4*x-8"
         }
+
     ]
 }
